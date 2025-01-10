7e9b8ad3f29ea9207a201d65aade87910872c7e8
Dowiw <monjie12272@gmail.com>
2025-01-10 19:07:28 +0100

    made more changes

--- Calculator.java
+++ Calculator.java
@@ -18,8 +18,42 @@ public class Calculator {
             System.out.println("Enter your choice: ");
             int choice = scanner.nextInt();
 
-            
-        }
+            if (choice == 5) {
+                System.out.println("Thank you for using the program. Good bye!");
+                bufferCalculator = false;
+                break;
+            }
 
+            System.out.println("Enter first number: ");
+            double firstnum = scanner.nextDouble();
+
+            System.out.println("Enter second number: ");
+            double secondnum = scanner.nextDouble();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result: " + (firstnum + secondnum));
+                    break;
+
+                case 2:
+                System.out.println("Result: " + (firstnum - secondnum));
+                break;
+
+                case 3:
+                System.out.println("Result: " + (firstnum * secondnum));
+                break;
+
+                case 4:
+                if (secondnum != 0) {
+                System.out.println("Result: " + (firstnum / secondnum));
+                } else {
+                    System.out.println("Error. Division by 0 is not allowed.");
+                }
+                break;
+                default:
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        scanner.close();
     }
 }
